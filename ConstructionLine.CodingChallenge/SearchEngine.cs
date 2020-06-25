3934c4f5273e986509f3fcab8c60723a6c0907c1
Tom Brereton <tombrereton@MacBook-Pro.local>
2020-06-26 00:55:02 +0100

    Refactor and clean search engine

--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -6,112 +6,90 @@ namespace ConstructionLine.CodingChallenge
 {
     public class SearchEngine
     {
-        private readonly List<Shirt> _shirts;
         private readonly Dictionary<Size, List<Shirt>> _shirtBySize;
         private readonly Dictionary<Color, List<Shirt>> _shirtsByColor;
 
         public SearchEngine(List<Shirt> shirts)
         {
-            _shirts = shirts;
-            _shirtBySize = new Dictionary<Size, List<Shirt>>();
-            _shirtsByColor = new Dictionary<Color, List<Shirt>>();
-
-            foreach (var shirt in shirts)
-            {
-                if (!_shirtBySize.TryAdd(shirt.Size, new List<Shirt> {shirt}))
-                {
-                    var existingList = _shirtBySize[shirt.Size];
-                    existingList.Add(shirt);
-                    _shirtBySize[shirt.Size] = existingList;
-                }
-
-                if (!_shirtsByColor.TryAdd(shirt.Color, new List<Shirt> {shirt}))
-                {
-                    var existingList = _shirtsByColor[shirt.Color];
-                    existingList.Add(shirt);
-                    _shirtsByColor[shirt.Color] = existingList;
-                }
-            }
+            var shirtsGroupedBySize = shirts.ToLookup(shirt => shirt.Size);
+            var shirtsGroupedByColor = shirts.ToLookup(shirt => shirt.Color);
+            _shirtBySize = shirtsGroupedBySize.ToDictionary(group => group.Key, group => group.ToList());
+            _shirtsByColor = shirtsGroupedByColor.ToDictionary(group => group.Key, group => group.ToList());
         }
-
 
         public SearchResults Search(SearchOptions options)
         {
-            // shirts
+            var shirtsBySize = GetShirtsBySize(options);
+            var shirtsByColor = GetShirtByColor(options);
+            var shirts = GetShirtsBySizeAndColor(shirtsBySize, shirtsByColor);
+
+            var sizeCounts = GetSizeCounts(shirts);
+            var colorCounts = GetColorCounts(shirts);
+
+            return new SearchResults
+            {
+                Shirts = shirts,
+                SizeCounts = sizeCounts,
+                ColorCounts = colorCounts
+            };
+        }
+
+        private List<Shirt> GetShirtsBySize(SearchOptions options)
+        {
             var shirtsBySize = new List<Shirt>();
-            var shirtsByColor = new List<Shirt>();
             foreach (var size in options.Sizes)
             {
                 shirtsBySize.AddRange(_shirtBySize[size]);
             }
 
+            return shirtsBySize;
+        }
+
+        private List<Shirt> GetShirtByColor(SearchOptions options)
+        {
+            var shirtsByColor = new List<Shirt>();
             foreach (var color in options.Colors)
             {
                 shirtsByColor.AddRange(_shirtsByColor[color]);
             }
 
-            var shirts = shirtsBySize;
-            if (!shirtsBySize.Any())
-            {
-                shirts = shirtsByColor;
-            }
+            return shirtsByColor;
+        }
 
+        private static List<Shirt> GetShirtsBySizeAndColor(List<Shirt> shirtsBySize, List<Shirt> shirtsByColor)
+        {
+            var shirts = shirtsBySize.Any() ? shirtsBySize : shirtsByColor;
             if (shirtsBySize.Any() && shirtsByColor.Any())
             {
                 shirts = shirtsBySize.Intersect(shirtsByColor).ToList();
             }
 
-            // size counts
-            var sizeLookup = shirts.ToLookup(s => s.Size);
-            var sizeCounts = new Dictionary<Size, SizeCount>();
-            foreach (var size in Size.All)
+            return shirts;
+        }
+
+        private List<SizeCount> GetSizeCounts(IEnumerable<Shirt> shirts)
+        {
+            var shirtsGroupedBySize = shirts.ToLookup(shirt => shirt.Size);
+            var sizeCounts = Size.All.ToDictionary(size => size, size => new SizeCount {Size = size, Count = 0});
+            foreach (var sizeGroup in shirtsGroupedBySize)
             {
-                sizeCounts[size] = new SizeCount {Size = size, Count = 0};
-                if (sizeLookup.Contains(size))
-                {
-                    var count = sizeLookup[size].Count();
-                    sizeCounts[size] = new SizeCount {Size = size, Count = count};
-                }
+                sizeCounts[sizeGroup.Key] = new SizeCount {Size = sizeGroup.Key, Count = sizeGroup.Count()};
             }
 
-            // color counts
-            var groupByColor = shirts.ToLookup(shirt => shirt.Color);
-            var colorCounts = new Dictionary<Color, ColorCount>();
-            foreach (var color in Color.All)
+            return sizeCounts.Values.ToList();
+        }
+
+        private static List<ColorCount> GetColorCounts(IEnumerable<Shirt> shirts)
+        {
+            var shirtsGroupedByColor = shirts.ToLookup(shirt => shirt.Color);
+            var colorCounts =
+                Color.All.ToDictionary(color => color, color => new ColorCount {Color = color, Count = 0});
+            foreach (var colorGroup in shirtsGroupedByColor)
             {
-                colorCounts[color] = new ColorCount {Color = color, Count = 0};
-                if (groupByColor.Contains(color))
-                {
-                    var count = groupByColor[color].Count();
-                    colorCounts[color] = new ColorCount {Color = color, Count = count};
-                }
+                colorCounts[colorGroup.Key] = new ColorCount {Color = colorGroup.Key, Count = colorGroup.Count()};
             }
 
-
-            // foreach (var color in Color.All)
-            // {
-            //     colorCounts[color] = new ColorCount {Color = color, Count = 0};
-            //     if (_shirtsByColor.ContainsKey(color))
-            //     {
-            //         var shirtsByOneColor = _shirtsByColor[color];
-            //         var colorCount = new ColorCount {Count = shirtsByOneColor.Count, Color = color};
-            //         if (shirtsBySize.Any())
-            //         {
-            //             var filteredShirtsByOneColor = shirtsByOneColor.Intersect(shirtsBySize);
-            //             colorCount = new ColorCount {Count = filteredShirtsByOneColor.Count(), Color = color};
-            //         }
-            //
-            //         colorCounts[color] = colorCount;
-            //     }
-            // }
-
-
-            return new SearchResults
-            {
-                Shirts = shirts,
-                SizeCounts = sizeCounts.Values.ToList(),
-                ColorCounts = colorCounts.Values.ToList()
-            };
+            return colorCounts.Values.ToList();
         }
     }
 }
